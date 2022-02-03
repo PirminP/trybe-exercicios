081eaab187cf91c0855806d2a2c828ee0bad68b1
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-02-03 15:56:05 -0300

    Array file for exercise of lecture

---
+++ Fundamentos/Bloco-4-Intro_JavaSript-Logica/4.2/arrays.js
@@ -0,0 +1,46 @@
+//let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+//console.log(tasksList.length);
+
+//let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+//let searchForFirstTask = tasksList[0];
+//console.log(searchForFirstTask);
+
+//let searchForLastTask = tasksList[tasksList.length - 1];
+//console.log(searchForLastTask);
+
+
+//let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+//tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+//console.log(tasksList);
+
+//let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+//tasksList.pop();  // remove a última tarefa
+//tasksList.shift();  // remove a first tarefa
+//console.log(tasksList);
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask);
+
+//Exc1
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu [1];
+
+console.log(menuServices);*/
+
+//Exc2
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Exc3
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+console.log(menu.push('Contato'));
+console.log(menu);
