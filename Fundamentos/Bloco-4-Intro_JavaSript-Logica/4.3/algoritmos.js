15cd88abadf463eebc26a87d7d72cdf8c1d0cdba
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-02-04 19:46:40 -0300

    Fix exc 2,3&4 dos exc 4.3

--- Fundamentos/Bloco-4-Intro_JavaSript-Logica/4.3/algoritmos.js
+++ Fundamentos/Bloco-4-Intro_JavaSript-Logica/4.3/algoritmos.js
@@ -20,4 +20,44 @@ for (let i = 10; i > 0; i -= 1) {
   fatorial = fatorial * i;
 }
 console.log(fatorial);
+
 //2
+//algorithm reverse word: https://levelup.gitconnected.com/javascript-algorithm-reverse-words-9ddae3a3ba39
+let word = 'Sarah';
+let invert = '';
+invert = word.split("").reverse().join("");
+console.log(invert);
+
+//3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let bigWord = array[0];
+let smallWord = array[0];
+
+for (i = 0; i < array.length; i += 1){
+    if (array[i].length > bigWord.length)
+    bigWord = array[i];
+}
+for (i = 0; i < array.length; i += 1){
+    if (array[i].length < smallWord.length)
+    smallWord = array[i];
+}
+
+console.log(bigWord);
+console.log(smallWord);
+
+//4
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
