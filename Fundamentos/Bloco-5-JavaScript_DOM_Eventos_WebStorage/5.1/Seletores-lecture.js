9a3588aad6e646d05bd8e35345a75d35b622e257
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-02-09 16:06:13 -0300

    File js created for Seletores lecture

---
+++ Fundamentos/Bloco-5-JavaScript_DOM_Eventos_WebStorage/5.1/Seletores-lecture.js
@@ -0,0 +1,3 @@
+document.getElementById('page-title').innerText = 'James Bond';
+document.getElementById('second-paragraph').innerText = 'A lot of traditon!';
+document.getElementById('subtitle').innerText = 'Who will be the next James Bond?';
