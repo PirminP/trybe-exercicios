7a459b7eaa384abd1636008a398e07c2eb3c828d
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-09 19:59:44 -0300

    Exercise 1 & 2

---
+++ Fundamentos/Bloco-8-Higher_Order_Functions_JS_ES6/exercise/script.js
@@ -0,0 +1,27 @@
+//1
+
+const employeeGenerator = (fullName) => {
+  const email = fullName.toLowerCase().split(' ').join('_');
+  return { fullName, email: `${email}@trybe.com` };
+};
+
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Dina Mendes'),
+    id2: callback('Gabi Hutter'),
+    id3: callback('Doctor Bob'),
+  };
+  return employees;
+};
+console.log(newEmployees(employeeGenerator));
+
+//2
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
