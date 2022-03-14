9b61ebe287fe4ab7d706df45e094f0fd479cda2f
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-14 16:04:00 -0300

    lecture exercise of array reduce

---
+++ Fundamentos/Bloco-8-Higher_Order_Functions_JS_ES6/8.4/lecture/arrayReduce.js
@@ -0,0 +1,87 @@
+// for loop
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+let sumNumbers = 0;
+// A variável 'acumula', a cada iteração do for, o resultado da operação feita lá!
+
+for (let index = 0; index < numbers.length; index += 1) {
+  sumNumbers += numbers[index];
+}
+console.log(sumNumbers); // 113
+
+
+// .reduce
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const sumNumbers = numbers.reduce((result, number) => result + number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
+console.log(sumNumbers); // 113
+
+// Ou seja:
+
+const getSum = (result, number) => result + number;
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
+
+
+
+const collection = [1, 2, 3, 4, 5];
+
+const getSum = (accumulator, number) => {
+  console.log(accumulator); // 1 3 6 10
+  return accumulator + number;
+};
+
+const sumNumbers = collection.reduce(getSum);
+console.log(sumNumbers); // 15
+
+
+// Passar segundo parametro para reduce 
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 32 47 50 52 47 103
+  return result + number;
+};
+
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
+
+// com alteracao (getSum,10)
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 10 32 47 50 52 47 103
+  return result + number;
+  };
+const sumNumbers = numbers.reduce(getSum, 10); // Parâmetro adicionado ao reduce: o "0"
+console.log(sumNumbers); // 123
+
+// Another example Array.reduce
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+const bigger = numbers.reduce(getBigger, 100);
+console.log(bigger); //100
+
+// Filter and reduce
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0;
+const sumPair = (currentValue, number) => currentValue + number;
+
+const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+console.log(sumNumbers(numbers)); // 152
+
+// Same example, with just reduce
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const sumPair = (currentValue, number) => (
+  (number % 2 === 0) ? currentValue + number : currentValue
+);
+
+const sumNumbers = (array) => array.reduce(sumPair, 0);
+
+console.log(sumNumbers(numbers)); // 152
+
