d95ae835673d38da3a028ab6cb64531d4b75b35f
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-15 18:47:32 -0300

    Adiciona array destructing

---
+++ Fundamentos/Bloco-8-Higher_Order_Functions_JS_ES6/8.5/lecture/objectDest.js
@@ -0,0 +1,46 @@
+// Object Destructuring
+// Example 1
+// const product = {
+//     name: 'Smart TV Crystal UHD',
+//     price: '1899.05',
+//     seller: 'Casas de Minas',
+//   };
+
+// const { name, seller } = product;
+// console.log(seller);
+
+// Example 2 Object
+// definindo o objeto
+const character = {
+    name: 'Luke SkyWalker',
+    age: '53',
+    description: {
+      specieName: 'Human',
+      jedi: true,
+    },
+    homeWorld: {
+      name: 'Tatooine',
+      population: '200000',
+    },
+  };
+
+  // desestruturando o objeto:
+const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character;
+
+// imprimindo os valores:
+console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+
+// Example 3
+const student = {
+    a: 'Maria',
+    b: 'Turma B',
+    c: 'Matematica',
+  };
+  
+  const { a: name, b: classAssigned, c: subject } = student;
+  
+  console.log(name); // Maria
+  console.log(classAssigned); // Turma B
+  console.log(subject); // Matemática
+
+  
