15f4f7338f8661f47671677a49a7f608db5432e0
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-15 16:09:55 -0300

    Adiciona parameter rest

---
+++ Fundamentos/Bloco-8-Higher_Order_Functions_JS_ES6/8.5/lecture/parameterRest.js
@@ -0,0 +1,13 @@
+// Parameter rest
+// Example 1
+function quantosParams(...args) {
+    console.log('parâmetros:', args);
+    return `Você passou ${args.length} parâmetros para a função.`;
+  }
+  
+  console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+  console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+
+// Example 2
+const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sum(4, 7, 10, 20, 50)); // 88
