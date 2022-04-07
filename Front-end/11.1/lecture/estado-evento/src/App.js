ac3045323a194469e333f4f2ba1f3db48c0f73fc
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-07 20:02:24 -0300

    Add stata and event lecture

---
+++ Front-end/11.1/lecture/estado-evento/src/App.js
@@ -0,0 +1,68 @@
+// Acessando uma função nossa dentro da classe
+
+// import React from 'react';
+// import './App.css';
+
+// class App extends React.Component {
+//   handleClick() {
+//     console.log('Clicou!')
+//   }
+
+//   render() {
+//     return <button type="button" onClick={this.handleClick}>Meu botão</button>;
+//   }
+// }
+
+// export default App;
+
+// Constructor
+// import React from 'react';
+// import './App.css';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     // A função `super()` é chamada para garantir que a lógica interna do React rode **antes** da sua. Se não for assim, o código não funcionará
+//     super()
+//     // console.log(props);
+//     // Sua lógica extra vai aqui! O parâmetro `props` é opcional, para você acessar as props diretamente no construtor
+//   }
+
+//   render() {
+//     return <span>Meu componente!</span>
+//   }
+// }
+
+// export default App;
+
+// this
+// import React from 'react';
+// import './App.css';
+
+// class App extends React.Component {
+//   render() {
+//     console.log(this)
+//     return <span>Hello, world!</span>
+//   }
+// }
+
+// export default App;
+
+// Problem of this
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  handleClick() {
+    // Essa chamada ao `this` retorna `undefined`? !
+    console.log(this)
+    console.log('Clicou')
+  }
+
+  render() {
+    // Já essa chamada ao `this`, feita de dentro da função `render`, retorna o objeto que esperamos
+    console.log(this)
+    return <button onClick={this.handleClick}>Meu botão</button>
+  }
+}
+
+export default App;
