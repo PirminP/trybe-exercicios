a631753dd93372d8e08b381329d3c8d9039e9412
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-08 20:27:19 -0300

    Add forms react

---
+++ Front-end/11.2/lecture/forms/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      estadoFavorito: '',
+    };
+  }
+
+
+  // handleChange(event) {
+  //   this.setState({
+  //     estadoFavorito: event.target.value,
+  //   });
+  // }
+
+  // event handler:
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+  
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <form className="form">
+          <label>
+            Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
+              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+          </label>
+          <input
+            type="number"
+            name="idade"
+          />
+          <input
+            type="checkbox"
+            name="vaiComparecer"
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
