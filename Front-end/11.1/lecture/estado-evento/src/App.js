6c803b462b25050e198cac3eca1108c6c92a0cd7
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-08 13:59:46 -0300

    Add modified file

--- Front-end/11.1/lecture/estado-evento/src/App.js
+++ Front-end/11.1/lecture/estado-evento/src/App.js
@@ -48,19 +48,45 @@
 // export default App;
 
 // Problem of this
+// import React from 'react';
+// import './App.css';
+
+// class App extends React.Component {
+//   handleClick() {
+//     // Essa chamada ao `this` retorna `undefined`? !
+//     console.log(this)
+//     console.log('Clicou')
+//   }
+
+//   render() {
+//     // Já essa chamada ao `this`, feita de dentro da função `render`, retorna o objeto que esperamos
+//     console.log(this)
+//     return <button onClick={this.handleClick}>Meu botão</button>
+//   }
+// }
+
+// export default App;
+
+// Connect this with constructor() to combine with function
 import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   handleClick() {
-    // Essa chamada ao `this` retorna `undefined`? !
+    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
+    Com isso, podemos acessar as `props`, estado do componente (ainda vamos ver como!)
+    e tudo o mais daqui de dentro */
     console.log(this)
-    console.log('Clicou')
+    console.log('Clicou!')
   }
 
   render() {
-    // Já essa chamada ao `this`, feita de dentro da função `render`, retorna o objeto que esperamos
-    console.log(this)
     return <button onClick={this.handleClick}>Meu botão</button>
   }
 }
