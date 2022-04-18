97f8d7707d2933d491717f9cc514e38a52c133c8
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-04-18 12:09:26 -0300

    Add file for react router

---
+++ Front-end/12.2/router/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ComponentePai from "./Components";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='main'>
+        <ComponentePai>
+          <p>SUPIMPA</p>
+          <h1>BACANA</h1>
+          <span>INCRÍVEL</span>
+        </ComponentePai>
+      </div>
+    )
+  }
+}
+
+
+export default App;
