19e221257222d3d80c71f7fe3f23066ccc7eca9a
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-04 19:43:59 -0300

    Testing react

---
+++ Front-end/14.1/testes-react/src/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label htmlFor="id-email">
+        Email
+        <input id="id-email" type="email" />
+      </label>
+      <input id="btn-send" type="button" value="Enviar" />
+    </div>
+  );
+}
+
+export default App;
---
+++ Front-end/14.1/testes-react/src/App.test.js
@@ -0,0 +1,17 @@
+// App.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('Verificando se existe o campo Email.', () => {
+  render(<App />);
+  const inputEmail = screen.getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail).toHaveProperty('type', 'email');
+});
+
+test('Verificando se existe um botão', () => {
+  render(<App />);
+  const btn = screen.getByRole('button');
+  expect(btn).toBeInTheDocument();
+});
