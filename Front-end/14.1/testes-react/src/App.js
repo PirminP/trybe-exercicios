2825d0be3fa00be82433c209718eabf1e3ed4d47
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-05-06 19:21:45 -0300

    RTL final files

--- Front-end/14.1/testes-react/src/App.js
+++ Front-end/14.1/testes-react/src/App.js
@@ -20,6 +20,7 @@
 
 // App.js
 import React, { Component } from 'react';
+import ValidEmail from './ValidEmail';
 import './App.css';
 
 class App extends Component {
@@ -60,7 +61,7 @@ class App extends Component {
           onClick={ () => this.changeSaveEmail(email) }
         />
         <input id="btn-id" type="button" value="Voltar" />
-        <h2 data-testid="id-email-user">{`Valor: ${saveEmail}`}</h2>
+        <ValidEmail email={ saveEmail } />
       </div>
     );
   }
