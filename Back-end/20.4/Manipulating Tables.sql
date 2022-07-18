18bf75ea24f82ce709fd4c8bed111d43ffa190b2
PirminP <pirmin.pernsteiner@gmail.com>
2022-07-18 16:06:17 -0300

    Update sql file manipulating tables

--- Back-end/20.4/Manipulating Tables.sql
+++ Back-end/20.4/Manipulating Tables.sql
@@ -30,10 +30,76 @@ VALUES
     ('SCIENCE'),
     ('CULTURE');
     
--- SELECT * FROM sakila.staff;
+SELECT * FROM sakila.staff;
 UPDATE sakila.staff
-SET first_name = 'Gust'
-WHERE staff_id = 4;
+SET first_name = 'Thomas', last_name = 'Shelby'
+WHERE staff_id = 5;
+
+SELECT first_name FROM sakila.actor;
+UPDATE sakila.actor
+SET first_name = 'STEAVE'
+WHERE actor_id IN (1,2,3);
+
+SELECT first_name FROM sakila.actor;
+-- SET SQL_SAFE_UPDATES = 0; -- safe mode because of ELSE first_name
+UPDATE sakila.actor
+SET first_name = (
+CASE actor_id WHEN 1 THEN 'AVE'
+			WHEN 2 THEN 'DAVID'
+			WHEN 3 THEN 'CARO'
+		ELSE first_name
+END);
+
+SELECT * FROM sakila.staff;
+UPDATE sakila.staff
+SET password = 'YourPassword'
+WHERE active = 1
+ORDER BY last_update
+LIMIT 2;
+
+SELECT rental_rate, rating, length, replacement_cost FROM sakila.film;
+UPDATE sakila.film
+SET rental_rate = 25
+WHERE length > 100
+AND (rating = 'G' OR rating = 'PG' OR rating = 'PG-13')
+AND replacement_cost > 20;
+
+UPDATE sakila.film
+SET rental_rate = (
+    CASE
+        WHEN length BETWEEN 1 AND 100 THEN 10
+        WHEN length > 100 THEN 20
+    END
+);
+
+SELECT * FROM sakila.film_text; 
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM sakila.film_text
+WHERE title = 'ACADEMY DINOSAUR';
+
+SELECT * FROM sakila.actor;
+-- DELETE FROM sakila.actor
+-- WHERE first_name = 'GRACE'; -- Necessary to delete dependencies first: ID film_actor
+
+SELECT * FROM sakila.film_actor;
+DELETE FROM sakila.film_actor
+WHERE actor_id = 7;
+DELETE FROM sakila.actor
+WHERE first_name = 'GRACE';
+
+SELECT * FROM sakila.actor;
+SELECT actor_id
+FROM sakila.actor
+WHERE first_name = 'KARL';
+-- SELECT * FROM sakila.film_actor;
+DELETE FROM sakila.film_actor
+WHERE actor_id = 12;
+DELETE FROM sakila.actor
+WHERE first_name = 'KARL';
+
+SELECT * FROM sakila.film_text;
+DELETE FROM sakila.film_text
+WHERE DESCRIPTION LIKE '%saga%';
 
 
 
