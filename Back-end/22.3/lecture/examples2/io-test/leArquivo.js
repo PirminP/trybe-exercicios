5b6021e46840dde4dd995f10e34b13de75314389
PirminP <pirmin.pernsteiner@gmail.com>
2022-07-30 21:10:48 -0300

    Adding more lecture exercise about tests mocha, chai and sinon

---
+++ Back-end/22.3/lecture/examples2/io-test/leArquivo.js
@@ -0,0 +1,21 @@
+// 3. STEP Criate arq with function
+
+// module.exports = () => {
+//     //
+// }
+
+
+// 4. STEP implementation
+const fs = require('fs');
+
+function leArquivo(nomeDoArquivo) {
+  try {
+    const conteudoDoArquivo = fs.readFileSync(nomeDoArquivo, 'utf8');
+
+    return conteudoDoArquivo;
+  } catch (err) {
+    return null;
+  }
+}
+
+module.exports = leArquivo;
