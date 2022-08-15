ed3fe57f2fc1c683805452cfab315d39dbb6a2be
PirminP <pirmin.pernsteiner@gmail.com>
2022-08-15 17:47:54 -0300

    Add exercise of Controller & Service

--- Back-end/23.2/exercise/cep-lookup/index.js
+++ Back-end/23.2/exercise/cep-lookup/index.js
@@ -1,11 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 
+const bodyParser = require('body-parser');
+const Cep = require('./controllers/CepController');
+const errorMiddleware = require('./middlewares/error.js');
+
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong!' });
 });
+
+app.get('/cep/:cep', Cep.findAddressByCep);
+
+app.post('/cep', Cep.create);
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
