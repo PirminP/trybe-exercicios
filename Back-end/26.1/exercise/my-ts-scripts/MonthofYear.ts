42cc445a6a1726c7dd92c11b14b158c8f8fd80a9
PirminP <pirmin.pernsteiner@gmail.com>
2022-09-13 16:49:48 -0300

    Feat: Exercise part intro TS

---
+++ Back-end/26.1/exercise/my-ts-scripts/MonthofYear.ts
@@ -0,0 +1,18 @@
+// Exercise2
+
+enum MonthofYear {
+    JANEIRO = "Janeiro",
+    FEVEREIRO = "Fevereiro",
+    MARCO = "Março",
+    ABRIL = "Abril",
+    MAIO = "Maio",
+    JUNHO = "Junho",
+    JULHO = "Julho",
+    AGOSTO = "Agosto",
+    SETEMBRO = "Setembro",
+    OUTUBRO = "Outubro",
+    NOVEMBRO = "Novembro",
+    DEZEMBRO = "Dezembro",
+}
+
+export default MonthofYear
