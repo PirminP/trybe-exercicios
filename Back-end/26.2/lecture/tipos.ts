3febb0b97ef26a4d4243df30aa0a8b7577659fba
PirminP <pirmin.pernsteiner@gmail.com>
2022-09-13 22:21:20 -0300

    Add: updating lecture generics

--- Back-end/26.2/lecture/tipos.ts
+++ Back-end/26.2/lecture/tipos.ts
@@ -117,3 +117,57 @@ let teacher: Teacher = {
         return `Olá, eu sou ${this.fullName()} e leciono ${this.subject}`;
     }
 }
+
+
+//Generics
+function getArray<T>(items : T[]) : T[] {
+    return new Array<T>().concat(items);
+}
+
+let numberArray = getArray<number>([5, 10, 15, 20]);
+numberArray.push(25);
+// numberArray.push("This is not a number"); 
+
+let stringArray = getArray<string>(["Cats", "Dogs", "Birds"]);
+stringArray.push("Rabbits");
+// stringArray.push(30);
+
+
+function identity<T, U> (value: T, message: U) : T {
+    console.log(message);
+    return value
+}
+
+let returnNumber = identity<number, string>(100, "Olá");
+let returnString = identity<string, string>("100", "Mundo");
+let returnBoolean = identity<boolean, string>(true, "Olá, Mundo!");
+
+// Generics - Interface
+interface ProcessIdentity<T, U> {
+    (value: T, message: U): T;
+}
+
+function processIdentity<T, U> (value: T, message: U) : T {
+    console.log(message);
+    return value
+}
+
+let processor: ProcessIdentity<number, string> = processIdentity;
+let returnNum = processor(100, "Olá");
+
+//Generics - Class
+class ProcessIdentity<T, U> {
+    _value: T;
+    _message: U;
+    constructor(value: T, message: U) {
+        this._value = value;
+        this._message = message;
+    }
+    getIdentity() : T {
+        console.log(this._message);
+        return this._value
+    }
+}
+
+let processorC = new ProcessIdentity<number, string>(100, "Olá");
+processorC.getIdentity();  // imprime "Olá" e retorna 100
