b20fc0d115bc6d80640e81cae44f38ab645d093e
PirminP <pirmin.pernsteiner@gmail.com>
2023-05-26 13:00:28 -0300

    Final exc of day 1

--- CS/IntroPython/exercise/exercises.py
+++ CS/IntroPython/exercise/exercises.py
@@ -6,7 +6,7 @@ def biggerNum(num1, num2):
     return num2
 
 
-print('Bigger number: ', biggerNum(6, 9))
+print('Bigger number:', biggerNum(6, 9))
 
 
 # exe 2: Calcule a média aritmética dos valores contidos em uma lista.
@@ -19,3 +19,19 @@ def mean(numbers):
 
 
 print(mean([1, 2, 3]))
+
+
+# exe 4: Crie uma função que receba uma lista de nomes
+# e retorne o nome com a maior quantidade de caracteres
+
+
+def return_biggest_name(names):
+    biggest_name = names[0]
+    for name in names:
+        if len(name) > len(biggest_name):
+            biggest_name = name
+    return biggest_name
+
+
+print('Biggest name:', return_biggest_name(['Stefan', 'Christoph',
+                                            'Michael', 'Johannes']))
