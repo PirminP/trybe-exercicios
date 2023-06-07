92a0de8ea98fbfc5fd1bfd87945b58e04bde093b
PirminP <pirmin.pernsteiner@gmail.com>
2023-06-07 19:27:27 -0300

    Node & linked List lecture content

---
+++ CS/EstruturaDadosI/day3/lecture/linked_list_content.py
@@ -0,0 +1,27 @@
+from node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_value = None
+        self.__length = 0
+
+    def __str__(self):
+        return f"LinkedList(len={self.__length}, value={self.head_value})"
+
+    def __len__(self):
+        return self.__length
+
+    def insert_first(self, value):
+        first_value = Node(value)
+        self.head_value = first_value
+        self.__length += 1
+
+    def insert_last(self, value):
+        last_value = Node(value)
+        current_value = self.head_value
+
+        while current_value.next:
+            current_value = current_value.next
+        current_value.next = last_value
+        self.__length += 1
